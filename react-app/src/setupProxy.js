5c33a10c92980c11fb194de199e83ea9be04864e
Hima Satwika Katragadda <83648082+JustARandomDude4@users.noreply.github.com>
2023-05-22 20:36:20 +0530

    added api

---
+++ react-app/src/setupProxy.js
@@ -0,0 +1,12 @@
+//auto complete template for proxy setup
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function (app) {
+  app.use(
+    '/api/*',
+    createProxyMiddleware({
+      target: 'http://localhost:7071/',
+      changeOrigin: true,
+    })
+  );
+};
